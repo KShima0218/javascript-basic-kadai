f26285d98fab376ef4e40958cc736b946adfb9ad
KShima0218 <165528320+KShima0218@users.noreply.github.com>
2024-06-12 06:43:49 +0900

    課題9再提出

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,12 +1,14 @@
 const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 let num = 0;
 
-for(let i=0; i <= 17; i++){
+for(let i=0; i <= 15; i++){
     console.log(holidays[i]);
 }
 
+console.log('----------------------------------');
 
-while(num !== 17){
+
+while(num !== 16){
     console.log(holidays[num]);
     num++;
 }
